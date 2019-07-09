51adcca1caeb4d65041d57f1bc7e7f4e23e06faa
Vanina Diot <vanina.diot@gmail.com>
2019-07-09 17:52:16 +0200

    initialisation du projet

---
+++ wild-spirit/src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @return Response
+     */
+
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
